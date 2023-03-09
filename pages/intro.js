4b340beaec5e464c80e006c4245a51ffac58fde0
mariia.synelnyk <44849051+Mariesnlk@users.noreply.github.com>
2023-03-09 16:10:27 +0100

    add first version of into page (fav projects)

--- pages/intro.js
+++ pages/intro.js
@@ -1,10 +1,31 @@
 import React from "react";
 import ContainerBlock from "../components/ContainerBlock";
+import FavouriteProjects from "../components/FavouriteProjects";
+import LatestCode from "../components/LatestCode";
+import Hero from "../components/Hero";
+import getLatestRepos from "@lib/getLatestRepos";
+import userData from "@constants/data";
 
-export default function intro() {
+export default function intro({ repositories }) {
   return (
     <ContainerBlock title="Mariia Synelnyk - Blockchain Developer">
-
+        <Hero/>
+        <FavouriteProjects/>
+        <LatestCode repositories={repositories}/>
     </ContainerBlock>
   );
 }
+
+export const getServerSideProps = async () => {
+    console.log(process.env.GITHUB_AUTH_TOKEN);
+    let token = process.env.GITHUB_AUTH_TOKEN;
+  
+    const repositories = await getLatestRepos(userData, token);
+    // console.log("REPOSITORIES", repositories);
+  
+    return {
+      props: {
+        repositories,
+      },
+    };
+  };
